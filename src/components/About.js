9f38efe8e228d100450c9657f3e40203b5d84a25
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-08-02 12:57:21 +0200

    second commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About page</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio dolorum. Placeat, repudiandae fugiat. A minima fugit voluptatem doloribus illo quis obcaecati, voluptates veniam, molestias aliquam tempora, minus debitis voluptate?</p>
+        </div>
+    )
+}
+
+export default Rainbow (About)
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = (props) => {
+    
+    return (
+        <div className="container">
+            <h4 className="center">Contact page</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio dolorum. Placeat, repudiandae fugiat. A minima fugit voluptatem doloribus illo quis obcaecati, voluptates veniam, molestias aliquam tempora, minus debitis voluptate?</p>
+        </div>
+    )
+}
+
+export default Contact
